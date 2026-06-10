db0b4b95c3ad618411e45215ec0e8aa1c127417b
Leonal Robin D <leonal2003lr10@gmail.com>
2026-06-10 20:00:31 +0530

    feat: register task routes in express app

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const taskRoutes = require('./routes/taskRoutes');
+app.use('/tasks', taskRoutes);
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB error:', err));
